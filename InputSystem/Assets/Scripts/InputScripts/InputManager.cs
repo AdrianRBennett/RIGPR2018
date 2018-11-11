fe38d35f9730e906cd82324f6ebb15ffe86bad71
Adrian <bennettadrian90@yahoo.co.uk>
2018-11-11 15:14:14 +0000

    InputSystem Commit 2 (Work-in-Progress)

---
+++ InputSystem/Assets/Scripts/InputScripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	public string[] latestInputWordList;
+	public UnityEngine.UI.Text testText;
+
+	//PRIVATE VARIABLES
+	 
+	private char[] splittingCharacters = new char[] {	// The input string is split at each instance of these characters.
+		' '
+	};
+
+	void Start () {
+		latestInputWordList = new string[] { "default" };
+	}
+
+	void Update () {
+		testText.text = latestInputWordList [0];
+
+	}
+
+	public void SplitInputToWordList(string input) {
+
+		latestInputWordList = input.Split (splittingCharacters);
+
+		if (latestInputWordList.Length >= 2) {
+			int test = 0;
+		}
+	}
+}
