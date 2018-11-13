639a684af2e72113f64b5b1296726c8eb0b4ecf5
Thai Daye <Up763322@myport.ac.uk>
2018-11-13 15:37:56 +0000

    Uploaded Unity Project

---
+++ ParadoxAtHursleyManor/Assets/CameraMove.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+
+    public float speed = 10.0f;     // Speed of xyz movement
+    public float maxSpeed = 20.0f;  // Maximum speed
+    private float oldSpeed;         // Temporary speed value
+
+    public float speedH = 2.0f;     // Speed value for the mouse movement
+    public float speedV = 2.0f;     // Speed value for the mouse movement
+
+    private float yaw = 0.0f;       // Current Y rotation of the camera
+    private float pitch = 0.0f;     // Current X rotation of the camera
+
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState = CursorLockMode.None; // Unlocks the cursor at start of game;
+        oldSpeed = speed;                       // Sets up the temporary value;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Boosts movement speed to max while left shift is held
+        if (Input.GetKey(KeyCode.LeftShift))   
+        {
+            if(speed != maxSpeed)
+            {
+                oldSpeed = speed;
+                speed = maxSpeed;
+            }
+        }
+        else // Resets speed back to normal when left shift is released
+        {
+            speed = oldSpeed;
+        }
+        // Keyboard (Forward Back, Straffe, Up Down)
+        float translation = Input.GetAxis("Vertical") * speed;  // Detects when vertical inputs are pressed. Positive (W, UpArrow), Negative (S, DownArrow) 
+        float straffe = Input.GetAxis("Horizontal") * speed;    // Detects when horizontal inputs are pressed. Positive (A), Negative (D) 
+        float rise = Input.GetAxis("Jump") * speed;             // Detects when Jump inputs are pressed. Positive (Space, R), Negative (F) 
+        translation *= Time.deltaTime;                          // Multiplies by delta time to keep it smooth
+        straffe *= Time.deltaTime;                              // ...
+        rise *= Time.deltaTime;                                 // ...
+
+        // Translates the camera
+        transform.Translate(straffe, rise, translation);        
+
+
+        // Rotates the camera based on cursor movement while Fire2 (Right mouse, left alt) is held
+        if (0 != Input.GetAxis("Fire2"))
+        {
+            // Locks the cursor to the center of the game screen
+            Cursor.lockState = CursorLockMode.Locked;
+
+            yaw += speedH * Input.GetAxis("Mouse X");   // Adds input to the yaw based on input
+            pitch -= speedV * Input.GetAxis("Mouse Y"); // Adds input to the pitch based on input
+
+            // Rotates camera based on mouse movement
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+        else 
+        {
+            // Unlocks the cursor while Fire2 is not pressed
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        
+	}
+}
---
+++ ParadoxAtHursleyManor/Assets/Scripts/LaserEmitter_Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserEmitter_Script : MonoBehaviour {
+
+    public GameObject Laser_Origin;
+
+    public float Max_RayCast;
+
+    public GameObject Laser_Cylinder;
+
+    void CreateLaser() {
+        RaycastHit hit;
+        if (Physics.Raycast(Laser_Origin.transform.position, Laser_Origin.transform.forward, out hit)) {
+            if (hit.collider.gameObject.GetComponent<Mirror_Script>() != null)
+            {
+                hit.collider.gameObject.GetComponent<Mirror_Script>().ReflectLaser(hit, Laser_Origin.transform.forward);
+            } else if (hit.collider.gameObject.GetComponent<Activate_On_Trigger>() != null)
+            {
+                hit.collider.gameObject.GetComponent<Activate_On_Trigger>().ChangeMaterial();
+            }
+
+        }
+        Laser_Cylinder.transform.position = Laser_Origin.transform.position;
+        Laser_Cylinder.transform.LookAt(hit.point);
+        Laser_Cylinder.transform.localScale = new Vector3(1.0f, 1.0f, (hit.distance * 5.0f));
+        Debug.DrawLine(Laser_Origin.transform.position, hit.point, Color.blue, 0.1f);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CreateLaser();
+	}
+}
