21d379efef55ce4c99259a384d7b2d81bfad24c6
Oscar Dunning <ozlodunning@gmail.com>
2018-11-17 08:57:38 +0000

    Added the scripts necessary to rotate an object on inptut. Currently these have not been attached to any objects but the script says which object to attached them to.

---
+++ ParadoxAtHursleyManor/Assets/Scripts/Interaction_and_rotation_scripts/Interact_Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact_Script : MonoBehaviour {
+
+	private RaycastHit hit;
+
+	// Much like the laser script, this code creates a raycast then activates a certain function on that object.
+    // Unlike the mirror script activate the code depending on the input provided:
+    // (All 45 degrees) L = Right, J = Left, I = Up, K = Down.
+	void Update () {
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 10.0f))
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                hit.collider.gameObject.GetComponent<Object_Rotate>().RotateObjectRight();
+            }
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                hit.collider.gameObject.GetComponent<Object_Rotate>().RotateObjectLeft();
+            }
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                hit.collider.gameObject.GetComponent<Object_Rotate>().RotateObjectUp();
+            }
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                hit.collider.gameObject.GetComponent<Object_Rotate>().RotateObjectDown();
+            }
+
+        }
+	}
+}
---
+++ ParadoxAtHursleyManor/Assets/Scripts/Interaction_and_rotation_scripts/Object_Rotate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Rotate : MonoBehaviour {
+
+    private Quaternion target;
+
+    private void Start()
+    {
+        target = transform.rotation;
+    }
+
+    private void Update()
+    {
+        Quaternion current = transform.rotation;
+        transform.rotation = Quaternion.Slerp(current, target, Time.deltaTime);
+    }
+
+    public void RotateObjectRight()
+    {
+        target.eulerAngles.Set(target.eulerAngles.x,(target.eulerAngles.y + 45), target.eulerAngles.z);
+        transform.Rotate(Vector3.up * 45);
+    }
+
+    public void RotateObjectLeft()
+    {
+        transform.Rotate(Vector3.up * -45);
+    }
+
+    public void RotateObjectUp()
+    {
+        transform.Rotate(Vector3.right * 45);
+    }
+
+    public void RotateObjectDown()
+    {
+        transform.Rotate(Vector3.right * -45);
+    }
+}
