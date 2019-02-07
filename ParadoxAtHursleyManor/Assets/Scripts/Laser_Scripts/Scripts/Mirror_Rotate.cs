4fefb74e1ad870905e88d345aed84769cf323e27
Oscar Dunning <ozlodunning@gmail.com>
2019-02-07 13:22:43 +0000

    Minor Changes

--- ParadoxAtHursleyManor/Assets/Scripts/Laser_Scripts/Scripts/Mirror_Rotate.cs
+++ ParadoxAtHursleyManor/Assets/Scripts/Laser_Scripts/Scripts/Mirror_Rotate.cs
@@ -6,26 +6,26 @@ public class Mirror_Rotate : MonoBehaviour
 {
     private bool isRotating = false;
    
-   void Update()
-    {
-        //if (Input.GetKeyDown(KeyCode.Z) && isRotating == false)
-        //{
-        //    isRotating = true;
-        //    StartCoroutine(Rotate(Vector3.up, 45, 2.0f));
-        //}
-        //if (Input.GetKeyDown(KeyCode.X) && isRotating == false)
-        //{
-        //    isRotating = true;
-        //    StartCoroutine(Rotate(Vector3.up, -45, 2.0f));
-        //}
-    }
+    //void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Z) && isRotating == false)
+    //     {
+    //         isRotating = true;
+    //         StartCoroutine(Rotate(Vector3.up, 45, 2.0f));
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.X) && isRotating == false)
+    //     {
+    //         isRotating = true;
+    //         StartCoroutine(Rotate(Vector3.up, -45, 2.0f));
+    //     }
+    //}
 
     public void RotateLeft()
     {
         if(isRotating == false)
         {
             isRotating = true;
-            StartCoroutine(Rotate(Vector3.up, 45, 2.0f));
+            StartCoroutine(Rotate(Vector3.up, 45, 1.0f));
         }
     }
 
@@ -34,13 +34,12 @@ public class Mirror_Rotate : MonoBehaviour
         if (isRotating == false)
         {
             isRotating = true;
-            StartCoroutine(Rotate(Vector3.up, -45, 2.0f));
+            StartCoroutine(Rotate(Vector3.up, -45, 1.0f));
         }
     }
 
-    IEnumerator Rotate(Vector3 axis, float angle, float duration = 1.0f)
+    IEnumerator Rotate(Vector3 axis, float angle, float duration)
     {
-        Debug.Log("Doing the thing!");
         Quaternion from = transform.rotation;
         Quaternion to = transform.rotation;
         to *= Quaternion.Euler(axis * angle);
